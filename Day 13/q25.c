819fd1b5e10816f84297720caf2a85c7d82c87f5
shloksingh121 <shaloksingh100@gmail.com>
2026-08-22 01:13:34 +0530

    Day 13

---
+++ Day 13/q25.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    char c;
+
+    scanf("%d %d %c", &a, &b, &c);
+
+    switch (c)
+    {
+        case '+':
+            printf("%d", a + b);
+            break;
+        case '-':
+            printf("%d", a - b);
+            break;
+        case '*':
+            printf("%d", a * b);
+            break;
+        case '/':
+            if (b != 0)
+                printf("%d", a / b);
+            else
+                printf("Division By Zero Is Not Allowed");
+            break;
+        case '%':
+            if (b != 0)
+                printf("%d", a % b);
+            else
+                printf("Division By Zero Is Not Allowed");
+            break;
+        default:
+            printf("Invalid Operator");
+    }
+
+    return 0;
+}
