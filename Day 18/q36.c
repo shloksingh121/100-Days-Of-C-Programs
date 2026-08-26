2d67e1e48c0480e90026e041e582dfab689f6aad
shloksingh121 <shaloksingh100@gmail.com>
2026-08-27 01:18:20 +0530

    Day 18

---
+++ Day 18/q36.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) 
+    {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+
+    printf("%d", a);
+    
+    return 0;
+}
