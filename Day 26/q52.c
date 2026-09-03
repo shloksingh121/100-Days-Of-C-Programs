e351cab2ec6dc30764ed1d659b3bf71a41296f9b
shloksingh121 <shaloksingh100@gmail.com>
2026-09-04 00:10:11 +0530

    Day 26

---
+++ Day 26/q52.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i = i + 2)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*\n");
+        }
+
+        printf("\n");
+    }
+
+    for (i = 3; i >= 1; i = i - 2)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*\n");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
